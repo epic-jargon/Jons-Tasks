36ff87d46f068b7c6be72d2feedacc978c98a931
Jamie <epic-jargon@hotmail.co.uk>
2018-06-27 00:34:27 +0100

    Start, code works but the regex isnt finished

---
+++ JonsTasks/T1_URLChecker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace JonsTasks
+{
+    class T1_URLChecker
+    {
+        private static String[] regex =
+            {
+                "^(https?|ftp|file):\\/\\/[-a-zA-Z0-9+&@#\\/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#\\/%=~_|]\\.(co.uk|com)"
+            };
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                String input = Console.ReadLine();
+
+                input = input.ToLower();
+                if (input == "exit")
+                    Environment.Exit(0);
+                else if (input == "help" || input == "")
+                    Console.WriteLine("help, exit");
+                else
+                {
+                    Console.WriteLine((checkIfRegexMatches(input)) ? "url is valid" : "url is not valid");
+                }
+            }
+        }
+        static bool checkIfRegexMatches(String input)
+        {
+            foreach (String s in regex)
+            {
+                Regex rgx = new Regex(s);
+                Match match = rgx.Match(input);
+                if (match.Success)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
